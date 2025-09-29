ff0a851a537744267fc16ae9755bedfd0c15266d
lenayoo <lenayoo17@gmail.com>
2025-09-29 15:16:42 +0900

    link to firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:very_berries/models/todo_item.dart';
 import 'helpers/storage_helper.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -14,7 +19,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final loc = AppLocalizations.of(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Very berries',
